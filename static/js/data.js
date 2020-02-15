942346cac2737d7fb6924a067c6a4fb865266204
qinmingjie <1934394876@qq.com>
2020-02-15 22:54:18 +0800

    banner no finished

--- static/js/data.js
+++ static/js/data.js
@@ -1,3 +1,11 @@
+let cloudMusic = {
+    banner:"banner",
+    recommend:"recommend",
+    newdic:"newdic",
+    notice:"notice",
+    new_top:"new_top",
+    original:"original"
+}
 // 封装Ajax函数请求git开源api来获取数据
 {
     function musicData(url){
@@ -12,14 +20,14 @@
     }
 
     //banner数据
-    let banner = musicData("http://39.107.87.215:8889/banner");
-    banner.then(function(data){
+    cloudMusic.banner = musicData("http://39.107.87.215:8889/banner");
+    cloudMusic.banner.then(function(data){
         //渲染视图
         let ban = document.querySelector(".banner");
         let par_ban = document.querySelector(".par-ban");
         let prev = document.querySelector(".rev-left");
         let preNext = document.querySelector(".rev-right");
-        console.log(data)
+        // console.log(data)
         //获取图片地址
         let Image = data.banners
         let banImgStr = ""
@@ -119,4 +127,147 @@
             },3000)
         }
     })
+    
+    //推荐歌单数据
+    cloudMusic.recommend = musicData("http://39.107.87.215:8889/personalized?limit=8");
+    cloudMusic.recommend.then(function(data){
+        let str = ""
+        let recommendUl = document.querySelector(".rec-mucWrap");
+        let newdata = data.result;
+        // console.log(data)
+        newdata.forEach(function(item,index){
+            let read = String(item.playCount).length>4?Math.floor((item.playCount)/10000)+"万":item.playCount
+            str += `
+            <li>
+                <div class="rec-muc">
+                    <img src="${item.picUrl}" alt="">
+                    <a href="" class="cover" id="${item.id}"></a>
+                    <div class="rec-icon">
+                        <span class="set-icon"></span>
+                        <span class="icon-txt">${read}</span>
+                        <a href="" class="p-icon"></a>
+                    </div>
+                    <p class="rec-mucTxt">
+                        <a href="">
+                            ${item.name}
+                        </a>
+                    </p>
+                </div>
+            </li>
+            `
+        })
+        recommendUl.innerHTML = str;
+    })
+
+    //新碟上架数据
+    cloudMusic.newdic = musicData("http://39.107.87.215:8889/album/newest");
+    cloudMusic.newdic.then(function(data){
+        console.log(data)
+        let newdicUl = document.querySelector(".newDic-Name")
+        let newdata = data.albums;
+        let str = ""
+        let num = 0;
+        let newStr = ""
+        newdata.forEach(function(item,index){
+            str += `
+            <li class="newDicList">
+                <div class="newDic-muc">
+                    <img src="${item.picUrl}" alt="">
+                    <a href="" class="newDic-cover"></a>
+                    <a href="" class="newDic-play"></a>
+                </div>
+                <p class="dictxt"><a href="" class="newDic-name">${item.name}</a></p>
+                <p class="dictxt"><a href="" class="newDic-txt">${item.artist.name}</a></p>
+            </li>
+            `
+        })
+        newdicUl.innerHTML = str
+    })
+
+    //榜单数据
+    //飙升榜
+    cloudMusic.notice = musicData("http://39.107.87.215:8889/top/list?idx=3");
+    cloudMusic.notice.then(function(data){
+        let newdata = data.playlist.tracks;
+        let lists = document.querySelector(".mucTop");
+        let toplog = document.querySelector(".Top-logo")
+        let logo = data.playlist.coverImgUrl;
+        toplog.innerHTML = `
+        <img src="${logo}">
+        <a href="" class="notice-cover"></a>
+        `
+        let str = "";
+        console.log(logo)
+        let num = 0;
+        newdata.forEach(function(item,index){
+            if(num<10){
+                str +=`
+                <li class="notice-list" >
+                    <span class="">${index+1}</span>
+                    <a href="" class="num-music">${item.name}</a>
+                    <div class="num-show"></div>
+                </li>
+                `
+            }
+            num++
+        })
+        lists.innerHTML = str;
+    })
+    // //新歌榜
+    cloudMusic.new_top = musicData("http://39.107.87.215:8889/top/list?idx=0");
+    cloudMusic.new_top.then(function(data){
+        let newdata = data.playlist.tracks;
+        let lists = document.querySelector(".notice-new-top");
+        let toplog = document.querySelector(".new-top")
+        let logo = data.playlist.coverImgUrl;
+        toplog.innerHTML = `
+        <img src="${logo}">
+        <a href="" class="notice-cover"></a>
+        `
+        let str = "";
+        console.log(logo)
+        let num = 0;
+        newdata.forEach(function(item,index){
+            if(num<10){
+                str +=`
+                <li class="notice-list " >
+                    <span class="">${index+1}</span>
+                    <a href="" class="num-music">${item.name}</a>
+                    <div class="num-show"></div>
+                </li>
+                `
+            }
+            num++
+        })
+        lists.innerHTML = str;
+    })
+    // //原创榜
+    cloudMusic.original = musicData("http://39.107.87.215:8889/top/list?idx=2");
+    cloudMusic.original.then(function(data){
+        let newdata = data.playlist.tracks;
+        let lists = document.querySelector(".original-top");
+        let toplog = document.querySelector(".original")
+        let logo = data.playlist.coverImgUrl;
+        toplog.innerHTML = `
+        <img src="${logo}">
+        <a href="" class="notice-cover"></a>
+        `
+        let str = "";
+        console.log(logo)
+        let num = 0;
+        newdata.forEach(function(item,index){
+            if(num<10){
+                str +=`
+                <li class="notice-list " >
+                    <span class="">${index+1}</span>
+                    <a href="" class="num-music">${item.name}</a>
+                    <div class="num-show"></div>
+                </li>
+                `
+            }
+            num++
+        })
+        lists.innerHTML = str;
+    })
+
 }
